ab1a3d7064b02c88bb09c549472ccd5c6b018a60
bacnguyen <bac03906@gmail.com>
2024-11-06 22:35:59 +0700

    lo

---
+++ Models/SideIncome/SideIncome.js
@@ -0,0 +1,9 @@
+const a = [
+    { id: 1, name: "Kinh doanh online" },
+    { id: 2, name: "Gia sư" },
+    { id: 3, name: "Làm freelancer" },
+    { id: 4, name: "Đầu tư tài chính" },
+    { id: 5, name: "Cho thuê nhà" },
+];
+
+localStorage.setItem('listSideIncome', JSON.stringify(a));
